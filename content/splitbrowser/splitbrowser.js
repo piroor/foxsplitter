3830668df6a75de3c7d8e5fbbaa631399e5b9488
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-01-24 06:01:58 +0000

    syntax error
    git-svn-id: http://www.cozmixng.org/repos/piro/splitbrowser/trunk@3529 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/splitbrowser/splitbrowser.js
+++ content/splitbrowser/splitbrowser.js
@@ -246,11 +246,11 @@ var SplitBrowser = {
 			);
 	},
  
-	getTabsArray : function(aTabBrowser)
+	getTabsArray : function(aTabBrowser) 
 	{
 		var tabs = this.getTabs(aTabBrowser);
 		var array = [];
-		for (var i = 0, maxi = tabs.snapshotLength; i < maxi)
+		for (var i = 0, maxi = tabs.snapshotLength; i < maxi; i++)
 		{
 			array.push(tabs.snapshotItem(i));
 		}
