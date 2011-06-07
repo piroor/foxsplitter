03e2cbe7d29d869bdee34f644d07fd02041c9e5c
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2011-06-07 23:46:10 +0900

    set position of new window automatically

--- restartless/modules/window.js
+++ restartless/modules/window.js
@@ -8,6 +8,17 @@ function FoxSplitterWindow(aWindow)
 	this.init(aWindow);
 }
 FoxSplitterWindow.prototype = {
+	kATTACHED_POSITION : 'foxsplitter-attached-position',
+	kPOSITION_TOP    : (1 << 0),
+	kPOSITION_RIGHT  : (1 << 1),
+	kPOSITION_BOTTOM : (1 << 2),
+	kPOSITION_LEFT   : (1 << 3),
+	kPOSITION_HORIZONTAL : (1 << 1) | (1 << 3),
+	kPOSITION_VERTICAL   : (1 << 0) | (1 << 2),
+	kPOSITION_OUTSIDE : (1 << 4),
+	kPOSITION_INSIDE  : (1 << 5),
+	kPOSITION_INVALID : (1 << 4) | (1 << 5),
+
 	isGroup : false,
 	lastScreenX : null,
 	lastScreenY : null,
@@ -19,11 +30,9 @@ FoxSplitterWindow.prototype = {
 
 		this.window = aWindow;
 
-		this.window.addEventListener('load', this, false);
-
-		this.window.addEventListener('unload', this, false);
-		this.window.addEventListener('DOMAttrModified', this, false);
-		this.window.addEventListener('resize', this, false);
+		aWindow.addEventListener('load', this, false);
+		aWindow.addEventListener('unload', this, false);
+		aWindow.addEventListener('dragend', this, true);
 
 		this._initParent();
 	},
@@ -36,7 +45,25 @@ FoxSplitterWindow.prototype = {
 		Deferred.next(function() {
 			self.lastScreenX = self.window.screenX;
 			self.lastScreenY = self.window.screenY;
+			self.startListen();
 		});
+	},
+
+	startListen : function FSW_startListen()
+	{
+		if (this._listening) return;
+		this.window.addEventListener('DOMAttrModified', this, false);
+		this.window.addEventListener('resize', this, false);
+		this._listening = true;
+	},
+	_listening : false,
+
+	endListen : function FSW_endListen()
+	{
+		if (!this._listening) return;
+		this.window.removeEventListener('DOMAttrModified', this, false);
+		this.window.removeEventListener('resize', this, false);
+		this._listening = false;
 	},
 
 	_initParent : function FSW_initParent()
@@ -46,12 +73,15 @@ FoxSplitterWindow.prototype = {
 				arguments &&
 				arguments.length > 0 &&
 				arguments[0] instanceof Ci.nsIDOMElement &&
-				arguments[0].localName == 'tab'
+				arguments[0].localName == 'tab' &&
+				arguments[0].hasAttribute(this.kATTACHED_POSITION)
 			) ? arguments[0].ownerDocument : null ;
 		var parentWin = parentDoc ? parentDoc.defaultView : null ;
 
 		if (!parentWin)
 			return;
+
+		this._initPositionAndSize(parentWin, parseInt(arguments[0].getAttribute(this.kATTACHED_POSITION)));
 
 		var parent = new FoxSplitterGroup();
 		parent.register(this);
@@ -62,6 +92,40 @@ FoxSplitterWindow.prototype = {
 			grandParent.unregister(parentWin.FoxSplitter);
 		}
 		parent.register(parentWin.FoxSplitter);
+	},
+
+	_initPositionAndSize : function FSW_initPositionAndSize(aParent, aPosition)
+	{
+		var w = aParent.window;
+		var x, y, width, height;
+		if (aPosition & this.kPOSITION_HORIZONTAL) {
+			y = w.screenY;
+			width = w.outerWidth * 0.5;
+			height = w.outerHeight;
+			if (aPosition == this.kPOSITION_LEFT) {
+				x = w.screenX - width;
+				aParent.moveBy(width, 0);
+			}
+			else {
+				x = w.screenX + width;
+			}
+			aParent.resizeBy(-width, 0);
+		}
+		else {
+			x = w.screenX;
+			width = w.outerWidth;
+			height = w.outerHeight * 0.5;
+			if (aPosition == this.kPOSITION_TOP) {
+				y = w.screenY - height;
+				aParent.moveBy(0, height);
+			}
+			else {
+				y = w.screenY + height;
+			}
+			aParent.resizeBy(0, -height);
+		}
+		this.window.moveTo(x, y);
+		this.window.resizeTo(width, height);
 	},
 
 	get root()
@@ -80,9 +144,12 @@ FoxSplitterWindow.prototype = {
 		if (this.parent)
 			this.parent.unregister(this);
 
-		this.window.removeEventListener('unload', this, false);
-		this.window.removeEventListener('DOMAttrModified', this, false);
-		this.window.removeEventListener('resize', this, false);
+		var w = this.window;
+		w.removeEventListener('unload', this, false);
+		w.removeEventListener('dragend', this, true);
+
+		this.endListen();
+
 		this.window = null;
 	},
 
@@ -97,6 +164,7 @@ FoxSplitterWindow.prototype = {
 			case 'unload':
 				return this.destroy();
 
+
 			case 'DOMAttrModified':
 				if (
 					aEvent.target == this.window.document.documentElement &&
@@ -107,6 +175,15 @@ FoxSplitterWindow.prototype = {
 
 			case 'resize':
 				return this.onResize(aEvent);
+
+
+			case 'dragend':
+				if (
+					aEvent.target.localName == 'tab' &&
+					aEvent.target.className.indexOf('tabbrowser-tab') > -1
+					)
+					this.onTabDragEnd(aEvent);
+				return;
 		}
 	},
 
@@ -135,6 +212,78 @@ FoxSplitterWindow.prototype = {
 		this.positionSynching = false;
 	},
 
+
+	onResize : function FSW_onResize(aEvent)
+	{
+	},
+
+
+	onTabDragEnd : function FSW_onTabDragEnd(aEvent)
+	{
+		var position = this.getDropPosition(aEvent);
+		if (position & this.kPOSITION_INVALID)
+			return;
+
+		var tab = this.getTabFromEvent(aEvent);
+		tab.setAttribute(this.kATTACHED_POSITION, position);
+	},
+
+	getTabFromEvent : function FSW_getTabFromEvent(aEvent)
+	{
+		var node = aEvent.originalTarget;
+		var d = node.ownerDocument;
+		return d.evaluate(
+				'ancestor-or-self::*[local-name()="tab" and contains(concat(" ", @class, " "), " tabbrowser-tab ")][1]',
+				node,
+				null,
+				Ci.nsIDOMXPathResult.FIRST_ORDERED_NODE_TYPE,
+				null
+			).singleNodeValue;
+	},
+
+	getDropPosition : function FSW_getDropPosition(aEvent)
+	{
+		var outerPadding = 100;
+		var oX = this.window.screenX - outerPadding;
+		var oY = this.window.screenY - outerPadding;
+		var x = aEvent.screenX - oX;
+		var y = aEvent.screenY - oY;
+		var width = this.window.outerWidth + (outerPadding * 2);
+		var height = this.window.outerHeight + (outerPadding * 2);
+
+		// out of area
+		if (x < 0 || x > width || y < 0 || y > height)
+			return this.kPOSITION_OUTSIDE;
+
+		// too inside
+		var xUnit = width * 0.3;
+		var yUnit = height * 0.3;
+		if (
+			xUnit < x && width - xUnit > x &&
+			yUnit < y && height - yUnit > y
+			)
+			return this.kPOSITION_INSIDE;
+
+		var isTopLeft    = x <= width - (y * width / height);
+		var isBottomLeft = x <= y * width / height;
+
+		return (isTopLeft && isBottomLeft) ? this.kPOSITION_LEFT :
+			(isTopLeft && !isBottomLeft) ? this.kPOSITION_TOP :
+			(!isTopLeft && isBottomLeft) ? this.kPOSITION_BOTTOM :
+			this.kPOSITION_RIGHT ;
+	},
+
+
+	moveTo : function FSW_moveTo(aX, aY)
+	{
+		if (this.positionSynching) return;
+		this.positionSynching = true;
+		this.window.moveTo(aX, aY);
+		this.lastScreenX += (this.lastScreenX - aX);
+		this.lastScreenY += (this.lastScreenY - aY);
+		this.positionSynching = false;
+	},
+
 	moveBy : function FSW_moveBy(aDX, aDY)
 	{
 		if (this.positionSynching) return;
@@ -145,9 +294,20 @@ FoxSplitterWindow.prototype = {
 		this.positionSynching = false;
 	},
 
-
-	onResize : function FSW_onResize(aEvent)
+	resizeTo : function FSW_resizeTo(aW, aH)
 	{
+		if (this.sizeSynching) return;
+		this.sizeSynching = true;
+		this.window.resizeTo(aW, aH);
+		this.sizeSynching = false;
+	},
+
+	resizeBy : function FSW_resizeBy(aDW, aDH)
+	{
+		if (this.sizeSynching) return;
+		this.sizeSynching = true;
+		this.window.resizeBy(aDW, aDH);
+		this.sizeSynching = false;
 	},
 
 
@@ -181,4 +341,11 @@ FoxSplitterWindow.prototype = {
 		};
 	}
 };
-  
+
+FoxSplitterWindow.prototype.positionName = {};
+FoxSplitterWindow.prototype.positionName[FoxSplitterWindow.prototype.kPOSITION_TOP] = 'top';
+FoxSplitterWindow.prototype.positionName[FoxSplitterWindow.prototype.kPOSITION_RIGHT] = 'right';
+FoxSplitterWindow.prototype.positionName[FoxSplitterWindow.prototype.kPOSITION_BOTTOM] = 'bottom';
+FoxSplitterWindow.prototype.positionName[FoxSplitterWindow.prototype.kPOSITION_LEFT] = 'left';
+FoxSplitterWindow.prototype.positionName[FoxSplitterWindow.prototype.kPOSITION_INSIDE] = 'in';
+FoxSplitterWindow.prototype.positionName[FoxSplitterWindow.prototype.kPOSITION_OUTSIDE] = 'out';
